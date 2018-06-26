27cfac5352eb6c1a4a47b0c97b96fe5eec4a406c
Reginaldo Morikawa <reginaldo.morikawa@m4web.com.br>
2018-06-26 12:55:55 -0300

    backend ok

--- tripadvisor-scrapper.php
+++ tripadvisor-scrapper.php
@@ -1,43 +1,63 @@
 <?php
 
-header('Content-type: application/json');
+header('Content-type: application/json; charset=utf-8');
 
-$url = "https://www.tripadvisor.com.br/Restaurant_Review-g303628-d3261455-Reviews-Vila_Dionisio-Sao_Jose_Do_Rio_Preto_State_of_Sao_Paulo.html";
-$ch = curl_init();
-$timeout = 5;
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-$html = curl_exec($ch);
-curl_close($ch);
+if ( isset($_POST["url"]) ) {
 
-$dom = new DOMDocument();
+    $url = $_POST["url"];
+    $status = '';
 
-@$dom->loadHTML($html);
+    if( strstr($url, "Restaurant", true) ) {
+        $ch = curl_init();
+        $timeout = 5;
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        $html = curl_exec($ch);
+        if (curl_errno($ch)) {
+            echo 'Curl error: ' . curl_error($ch);
+        }
+        curl_close($ch);
 
-$classname_nome = "heading_title";
-$classname_telefone = "phone";
-$classname_reviews = "rating";
-$classname_imagens = "carousel";
+        $dom = new DOMDocument();
 
-$finder = new DomXPath($dom);
-$spaner_nome = $finder->query("//*[contains(@class, '$classname_nome')]");
-$spaner_telefone = $finder->query("//*[contains(@class, '$classname_telefone')]");
-$spaner_reviews = $finder->query("//*[contains(@class, '$classname_reviews')]");
-$spaner_imagens = $finder->query("//*[contains(@class, '$classname_imagens')]");
+        @$dom->loadHTML($html);
 
-$nome = trim(preg_replace('/\s+/', ' ', strip_tags($dom->saveHtml($spaner_nome[0]))));
-$telefone = trim(preg_replace('/\s+/', ' ', strip_tags($dom->saveHtml($spaner_telefone[0]))));
-$reviews = strtok(preg_replace('/\s+/', ' ', strip_tags($dom->saveHtml($spaner_reviews[0]))), " ");
-$imagens = $dom->saveHtml($spaner_imagens[0]);
+        $classname_nome = "heading_title";
+        $classname_telefone = "phone";
+        $classname_reviews = "rating";
+        $classname_imagens = "onDemandImg";
+
+        $finder = new DomXPath($dom);
+        $spaner_nome = $finder->query("//*[contains(@class, '$classname_nome')]");
+        $spaner_telefone = $finder->query("//*[contains(@class, '$classname_telefone')]");
+        $spaner_reviews = $finder->query("//*[contains(@class, '$classname_reviews')]");
+        $spaner_imagens = $finder->query("//*[contains(@class, '$classname_imagens')]");
+
+        $nome = trim(preg_replace('/\s+/', ' ', strip_tags($dom->saveHtml($spaner_nome[0]))));
+        $telefone = trim(preg_replace('/\s+/', ' ', strip_tags($dom->saveHtml($spaner_telefone[0]))));
+        $reviews = strtok(preg_replace('/\s+/', ' ', strip_tags($dom->saveHtml($spaner_reviews[0]))), " ");
+
+        $imagens = [];
+
+        foreach ($spaner_imagens as $spaner_imagem) {
+            $imagens[] = preg_replace('/\s+/', '', $spaner_imagem->getAttribute('data-src'));
+        }
 
 
-//foreach(array_slice($imagens->saveHtml('img[data-src]'), 0, 3) as $imgsrc ):
-//    $imglinks[] = $imgsrc;
+        $response = [nome => $nome, telefone => $telefone, reviews => $reviews, imagens => $imagens];
 
-$response = [$nome, $telefone, $reviews];
-//$response = [$nome, $telefone, $reviews, $imagens];
-//$response = [$nome, $telefone, $reviews, $imglinks];
+        $json = json_encode($response, JSON_UNESCAPED_SLASHES);
 
-echo json_encode( $response, JSON_UNESCAPED_UNICODE );
-//echo json_encode( $response);
+        echo $json;
+    }else{
+        $status = "Critério da pesquisa inválido, verifique os dados e tente novamente";
+        $response = [ status => $status];
+        $json = json_encode($response, JSON_UNESCAPED_SLASHES);
+
+        echo $json;
+    }
+}
+?>
